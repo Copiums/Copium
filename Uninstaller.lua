184d49fb2acd808d5d6b60edf6d17d1daf4f73df
Copium <119388242+Copiums@users.noreply.github.com>
2025-06-14 12:52:50 -0400

    Update Uninstaller.lua

--- Uninstaller.lua
+++ Uninstaller.lua
@@ -1,40 +1,18 @@
 --[[ Copium Uninstaller
 
      [VELOCITY]
-     Created By Sammz
+     Created By Copium
 
 ]]
-
-local lplr = game:GetService("Players").LocalPlayer
-local names = {'GuiLibrary.lua', 'MainScript.lua', 'Universal.lua'}
-local folder = 'vape'
-if not isfolder(folder) then
-    lplr:Kick("Are you retarded?")
-    return
-end
-function dfif(folderPath)
-    local files = listfiles(folderPath)
-    for _, fn in pairs(files) do
-        delfile(folderPath .. '/' .. fn)
-    end
-end
-function deleteFiles()
-    local ftd = {'commithash.txt', 'assetsversion.txt'}
-    for _, fn in pairs(ftd) do
-        if isfolder(fn) then
-              delfile(folder .. '/' .. fn)
-       end
-    end
-end
-for _, name in pairs(names) do
-    if isfile(name) then
-        delfile(folder .. '/' .. name)
-     end
-end
-deleteFiles()
-delfolder(folder .. '/' .. 'assets')
-delfolder(folder .. '/' .. 'Libraries')
-delfolder(folder .. '/' .. 'Profiles')
-delfolder(folder .. '/' .. 'CustomModules')
-delfolder(folder)
-lplr:Kick("Velocity: \n Uninstall success!")
+local copium: table = {};
+local cloneref: (obj: any) -> any = cloneref or function(obj)
+    return obj;
+end;
+local playersService: Players = cloneref(game:GetService('Players'));
+local lplr: Player = playersService.LocalPlayer;
+if isfolder("newvape") then
+     delfolder("newvape");
+else
+     lplr:Kick("Are you that dumb?");
+end;
+lplr:Kick("Velocity: \n Uninstall success!");
